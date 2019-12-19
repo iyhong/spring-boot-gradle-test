04f0c42d5979ef6e5687b1695c2b73b8072a80f3
inyonghong <loverman85@hanmail.net>
2019-12-20 00:03:23 +0900

    OAuth 설정 및 구현 완료

--- src/main/java/com/hong/springboot/web/IndexCotroller.java
+++ src/main/java/com/hong/springboot/web/IndexCotroller.java
@@ -1,24 +1,30 @@
 package com.hong.springboot.web;
 
+import com.hong.springboot.config.auth.dto.SessionUser;
 import com.hong.springboot.service.PostsService;
-import com.hong.springboot.web.domain.posts.PostsRepository;
 import com.hong.springboot.web.dto.PostsResponseDto;
-import com.sun.org.apache.xpath.internal.operations.Mod;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexCotroller {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
