de0343e482d9c011ece943ca98642a3bdc3de1be
inyonghong <loverman85@hanmail.net>
2019-12-21 19:26:29 +0900

    IndexCotroller 내부에 Session 에서 user정보를 가져오는 코드를 @annotation 으로 변경

--- src/main/java/com/hong/springboot/web/IndexCotroller.java
+++ src/main/java/com/hong/springboot/web/IndexCotroller.java
@@ -1,5 +1,6 @@
 package com.hong.springboot.web;
 
+import com.hong.springboot.config.auth.LoginUser;
 import com.hong.springboot.config.auth.dto.SessionUser;
 import com.hong.springboot.service.PostsService;
 import com.hong.springboot.web.dto.PostsResponseDto;
@@ -19,9 +20,8 @@ public class IndexCotroller {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if(user != null){
             model.addAttribute("userName", user.getName());
         }
