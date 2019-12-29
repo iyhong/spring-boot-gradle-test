6dad74316cb796caf082b6582bf96a795c1ba4e8
inyonghong <loverman85@hanmail.net>
2019-12-29 15:45:07 +0900

    profile.sh 수정

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -8,12 +8,12 @@ IDLE_PORT=$(find_idle_port)
 
 echo "> $IDLE_PORT 에서 구동중인 애플리케이션 pid 확인"
 IDLE_PID=$(lsof -ti tcp:${IDLE_PORT})
-
+echo "> $IDLE_PORT 에서 구동중인 애플리케이션 pid: ${IDLE_PID}"
 if [ -z ${IDLE_PID} ]
 then
   echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-  echo "> kill -15 $IDLE_IDLE_PID"
-  kill 15 ${IDLE_PID}
+  echo "> kill -15 $IDLE_PID"
+  kill -15 ${IDLE_PID}
   sleep 5
 fi
